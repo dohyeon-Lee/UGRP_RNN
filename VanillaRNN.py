648d2f2d55268e0800fcc50d3f0a77d168a9d970
dohyeon-Lee <dohyeon@postech.ac.kr>
2023-12-14 01:54:39 +0900

    save

--- VanillaRNN.py
+++ VanillaRNN.py
@@ -7,18 +7,19 @@ import torch.optim as optim
 
 class VanillaRNN(nn.Module):
 
-  def __init__(self, input_size, hidden_size, sequence_length, num_layers, device):
+  def __init__(self, input_size, hidden_size, num_layers, device):
     super(VanillaRNN, self).__init__()
     self.device = device
     self.hidden_size = hidden_size
     self.num_layers = num_layers
     self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
     # self.fc = nn.Sequential(nn.Linear(hidden_size * sequence_length, 2), nn.Tanh())
-    self.fc = nn.Linear(hidden_size * sequence_length, 2)
+    self.fc = nn.Linear(hidden_size, 2)
 
   def forward(self, x):
     h0 = torch.zeros(self.num_layers, x.size()[0], self.hidden_size).to(self.device) # 초기 hidden state 설정하기.
     out, _ = self.rnn(x, h0) # out: RNN의 마지막 레이어로부터 나온 output feature 를 반환한다. hn: hidden state를 반환한다.
-    out = out.reshape(out.shape[0], -1) # many to many 전략
     out = self.fc(out)
+    # out = out.reshape(out.shape[0], -1) # many to many 전략
+    # out = self.fc(out)
     return out
--- train_direct.py
+++ train_direct.py
@@ -47,9 +47,7 @@ loss_graph = [] # 그래프 그릴 목적인 loss.
 n = len(database.train_loader[0])
 
 for epoch in range(num_epochs):
-    running_loss1 = 0.0
-    running_loss2 = 0.0
-    running_loss3 = 0.0
+    running_loss = 0.0
     train_loader = database.train_loader[random.randrange(0,train_datasize)]
     for data in train_loader:
         
@@ -57,62 +55,22 @@ for epoch in range(num_epochs):
         seq, target = data #seq, target은 20개, seq는 3차원 배열 20*8*3 , target은 2차원 배열 20*2
         target[:,0] += np.pi/2
         out = model(seq)
-        loss1 = criterion(out, target)
-        
-        # loss 2 calculate
-        dt = 600/30000.
-        expected_theta_dot = torch.zeros(out[:,0].shape[0]).to(device)
-        bbefore_theta = 0
-        before_theta = 0
-        for idx, theta in enumerate(out[:,0]):
-            if idx > 1:
-                theta_dot = (theta - bbefore_theta)/(2*dt) 
-                expected_theta_dot[idx-1] = theta_dot
-            bbefore_theta = before_theta
-            before_theta = theta       
-        loss2 = criterion(expected_theta_dot[1:-1], target[1:-1,1])
+        loss = criterion(out, target)
 
-        # loss 3 calculate
-        g = 9.8 
-        L = 1.5 
-        m = 1.0
-        M = 2.0 
-        k = 8 # coefficients c/m
-        x_ddot = seq[1:,-1,0]
-        true_theta_ddot = -k*target[:-1,1]*torch.cos(target[:-1,0])-(g/L)*torch.sin(target[:-1,0])-(x_ddot/L)*torch.cos(target[:-1,0])
-        expected_theta_ddot = torch.zeros(out[:,0].shape[0]).to(device)
-        bbefore_theta_dot = 0
-        before_theta_dot = 0
-        for idx, theta_dot in enumerate(out[:,1]):
-            if idx > 1:
-                theta_ddot = (theta_dot - bbefore_theta_dot)/(2*dt) 
-                expected_theta_ddot[idx-1] = theta_ddot
-            bbefore_theta_dot = before_theta_dot
-            before_theta_dot = theta_dot       
-        loss3 = criterion(expected_theta_ddot[1:-1], true_theta_ddot[1:])
-
-        loss1 = 100*loss1
-        loss2 = 0.1*loss2
-        loss3 = 0.01*loss3
-        loss = loss1 # loss2 + loss3
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        running_loss1 += loss1.item()
-        running_loss2 += loss2.item()
-        running_loss3 += loss3.item()
-    running_loss = running_loss1 + running_loss2 + running_loss3
-    writer.add_scalar("Loss/epoch", running_loss, epoch)
+        running_loss += loss.item()
+
     loss_graph.append(running_loss / n) # 한 epoch에 모든 배치들에 대한 평균 loss 리스트에 담고,
     if epoch % 10 == 0:
-        print('[epoch: %d] loss1: %.4f loss2: %.4f loss3: %.4f'%(epoch, running_loss1 / n, running_loss2 / n, running_loss3 / n))
-writer.close()
+        print('[epoch: %d] loss: %.4f '%(epoch, running_loss / n))
 plt.figure()
 plt.plot(loss_graph)
 plt.show()
 
 ## model wieght save
-PATH = "model/train_direct_dict_batch_"+str(database.batch_size)+"_epoch_"+str(num_epochs)+"_loss1.pt"
+PATH = "model/train_direct_dict_batch_"+str(database.batch_size)+"_epoch_"+str(num_epochs)+".pt"
 torch.save(model.state_dict(), PATH)
 
 
