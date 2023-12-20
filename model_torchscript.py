15d6401b86b64688764d4a5f2fda6b60dedbee12
dohyeon-Lee <dohyeon@postech.ac.kr>
2023-12-20 14:55:57 +0900

    model add

---
+++ model_torchscript.py
@@ -0,0 +1,43 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import cv2
+from tqdm import tqdm
+import random
+from VanillaRNN import VanillaRNN
+from data_loader import data_loader
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(f'{device} is available')
+
+
+## parameters, dataset
+
+database = data_loader(num_epochs=200, device=device)
+
+## models
+input_size = 1 
+num_layers = 2
+hidden_size = 8
+model = VanillaRNN(input_size=input_size,
+                   hidden_size=hidden_size,
+                   num_layers=num_layers,
+                   device=device).to(device)
+
+
+hn = torch.rand(num_layers, 1, hidden_size).to(device)
+example = torch.rand(1).unsqueeze(0).unsqueeze(0).to(device)
+
+traced_script_module = torch.jit.trace(model, (example, hn))
+traced_script_module.save("model/ugrp_rnn_model.pt")
